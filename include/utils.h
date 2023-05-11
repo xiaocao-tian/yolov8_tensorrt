d8f7c1b740613354060235a101dbe4b539388947
tianqiang1223 <65889782+tianqiang1223@users.noreply.github.com>
2023-05-11 20:04:15 +0800

    Create utils.h

---
+++ include/utils.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "model.h"
+#include "NvInfer.h"
+#include <fstream>
+#include <iostream>
+#include <map>
+
+class Logger : public nvinfer1::ILogger {
+    void log(Severity seversity, const char* msg) noexcept override {
+        std::cout << msg << std::endl;
+    }
+};
+
+void readEngineFile(const std::string& engine_file, nvinfer1::IRuntime*& runtime, nvinfer1::ICudaEngine*& engine, 
+nvinfer1::IExecutionContext*& context);
+
+void readClassFile(const std::string& class_file, std::map<int, std::string>& labels);
