9ab74a710a2f079df6a1fc5cd906d0b0681a61b1
tianqiang1223 <65889782+tianqiang1223@users.noreply.github.com>
2023-05-11 20:09:36 +0800

    Create utils.cpp

---
+++ src/utils.cpp
@@ -0,0 +1,48 @@
+#include "utils.h"
+
+Logger ggLogger;
+
+
+void readEngineFile(const std::string& engine_file, nvinfer1::IRuntime*& runtime, nvinfer1::ICudaEngine*& engine, nvinfer1::IExecutionContext*& context){
+    std::fstream file;
+
+    file.open(engine_file, std::ios::in | std::ios::binary);
+
+    if(!file.is_open()){
+        std::cout << "Load engine file failed: " << engine_file << std::endl;
+		system("pause");
+		exit(0);
+    }
+    std::cout << "Load engine file success: " << engine_file << std::endl;
+	size_t size = 0;
+	file.seekg(0, file.end);
+	size = file.tellg();
+	file.seekg(0, file.beg);
+	char* serialized_engine = new char[size];
+	file.read(serialized_engine, size);
+	file.close();
+
+	runtime = nvinfer1::createInferRuntime(ggLogger);
+	engine = runtime->deserializeCudaEngine(serialized_engine, size);
+	context = engine->createExecutionContext();
+	delete[] serialized_engine;
+
+}
+
+
+void readClassFile(const std::string& class_file, std::map<int, std::string>& labels) {
+    std::fstream file(class_file, std::ios::in);
+    if (!file.is_open()) {
+        std::cout << "Load classes file failed: " << class_file << std::endl;
+        system("pause");
+        exit(0);
+    }
+    std::cout << "Load classes file success: " << class_file << std::endl;
+    std::string str_line;
+    int index = 0;
+    while (getline(file, str_line)) {
+        labels.insert({ index, str_line });
+        index++;
+    }
+    file.close();
+}
