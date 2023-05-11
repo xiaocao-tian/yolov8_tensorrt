50f2771c09113803034d4167759d2cfd1554f92c
tianqiang1223 <65889782+tianqiang1223@users.noreply.github.com>
2023-05-11 20:02:35 +0800

    create block.h

--- include/block.h
+++ include/block.h
@@ -4,7 +4,6 @@
 #include <string>
 #include "NvInfer.h"
 
-
 std::map<std::string, nvinfer1::Weights> loadWeights(const std::string file);
 
 nvinfer1::IElementWiseLayer* convBnSiLU(nvinfer1::INetworkDefinition* network, std::map<std::string, nvinfer1::Weights> weightMap, 
@@ -12,7 +11,6 @@ nvinfer1::ITensor& input, int ch, int k, int s, int p, std::string lname);
 
 nvinfer1::IElementWiseLayer* C2F(nvinfer1::INetworkDefinition* network, std::map<std::string, nvinfer1::Weights> weightMap, 
 nvinfer1::ITensor& input, int c1, int c2, int n, bool shortcut, float e, std::string lname);
-
 
 nvinfer1::IElementWiseLayer* SPPF(nvinfer1::INetworkDefinition* network, std::map<std::string, nvinfer1::Weights> weightMap, 
 nvinfer1::ITensor& input, int c1, int c2, int k, std::string lname);
